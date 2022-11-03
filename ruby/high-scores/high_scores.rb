b3b090914676b02a5ae308848b97fffa0bb5088b
Emmanuel Etti <emmanueletti@gmail.com>
2022-11-02 20:06:48 -0700

    Adds highscores solution

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,7 +1,32 @@
-=begin
-Write your code for the 'High Scores' exercise in this file. Make the tests in
-`high_scores_test.rb` pass.
+# Write your code for the 'High Scores' exercise in this file. Make the tests in
+# `high_scores_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/high-scores` directory.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/high-scores` directory.
-=end
+class HighScores
+  attr_reader :scores, :latest, :personal_best, :personal_top_three
+
+  def initialize(scores)
+    @scores = scores
+    @latest = scores.last
+    @personal_best = scores.max
+    @personal_top_three = scores.max(3)
+  end
+
+  # def latest
+  #   scores.last
+  # end
+
+  # def personal_best
+  #   scores.max
+  # end
+
+  # def personal_top_three
+  #   scores.max(3)
+  # end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
