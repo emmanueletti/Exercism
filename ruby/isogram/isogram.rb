2cff838eefe3a86473b54d65cb107d832c131b09
Emmanuel Etti <emmanueletti@gmail.com>
2022-11-04 18:27:28 -0700

    adds isogram

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,7 +1,12 @@
-=begin
-Write your code for the 'Isogram' exercise in this file. Make the tests in
-`isogram_test.rb` pass.
+# Write your code for the 'Isogram' exercise in this file. Make the tests in
+# `isogram_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/isogram` directory.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/isogram` directory.
-=end
+class Isogram
+  def self.isogram?(input)
+    sanitized_input = input.downcase.scan(/\w/).join
+    sanitized_input.chars.uniq.size == sanitized_input.size
+  end
+end
