fca484f758513c76b0a798eb3a536ffd95790212
Emmanuel Etti <emmanueletti@gmail.com>
2022-11-26 10:49:33 -0800

    Add

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -3,3 +3,34 @@
 #
 # To get started with TDD, see the `README.md` file in your
 # `ruby/sum-of-multiples` directory.
+
+class SumOfMultiples
+  def initialize(*args)
+    @nums = args
+    @multiples = []
+  end
+
+  def to(end_value)
+    (1...end_value).each do |candidate|
+      check_if_multiple(candidate)
+    end
+    multiples.sum
+  end
+
+  private
+
+  def check_if_multiple(candidate)
+    nums.each do |num|
+      if is_multiple_of?(num: num, candidate: candidate)
+        multiples.push(candidate)
+        break
+      end
+    end
+  end
+
+  def is_multiple_of?(num:, candidate:)
+    candidate % num == 0
+  end
+
+  attr_reader :nums, :multiples
+end
---
+++ ruby/transpose/transpose.rb
@@ -0,0 +1,25 @@
+# Write your code for the 'Transpose' exercise in this file. Make the tests in
+# `transpose_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/transpose` directory.
+
+# what is given
+# a string seperated by \n to denote rows
+
+# what do we want
+# a string where the rows have become the cols and the cols the rows
+
+# caveat
+# if the rows have diff lengths, then add the padding on the left
+
+class Transpose
+  def self.transpose(input)
+    # take the input string and represent it as a matrix
+    # take each line and as a row of strings
+    # turn each line into individual chars
+
+    # transpose
+    # take the matrix and
+  end
+end
