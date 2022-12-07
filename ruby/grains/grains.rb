0973af8badabe340c62f80555b8e6e2d8e18770b
Emmanuel Etti <emmanueletti@gmail.com>
2022-12-06 19:28:22 -0800

    Adds grains assignment

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,7 +1,24 @@
-=begin
-Write your code for the 'Grains' exercise in this file. Make the tests in
-`grains_test.rb` pass.
+# Write your code for the 'Grains' exercise in this file. Make the tests in
+# `grains_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your `ruby/grains`
+# directory.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/grains` directory.
-=end
+class Grains
+  class << self
+    def square(value)
+      raise ArgumentError if value <= 0 || value > 64
+      calculate_exponential_growth(value)
+    end
+
+    def total
+      (1..64).sum { |n| square(n) }
+    end
+
+    private
+
+    def calculate_exponential_growth(value)
+      1 * 2**(value - 1)
+    end
+  end
+end
